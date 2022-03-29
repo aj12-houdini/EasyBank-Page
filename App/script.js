53b2e73e224a9996f7eb036e52d352c3afbdee13
aayushjuyal <aayushjuyal12@gmail.com>
2022-03-29 23:58:46 +0530

    first commit

---
+++ App/script.js
@@ -0,0 +1,15 @@
+const collapse = document.querySelector(".collapse-img")
+const nav = document.querySelector(".nav-bar-list")
+const close = document.querySelector(".close")
+const homeSection = document.querySelector(".home-section")
+let collapseClicked = false
+collapse.addEventListener("click",function(){
+        close.classList.add("active")
+        nav.classList.add("responsive")
+})
+
+close.addEventListener("click",function(){
+    close.classList.remove("active")
+    nav.classList.remove("responsive")
+})
+
